ff9466e0a7c5a6eabfd595407a355337b6063807
sfs-inep <igornepipenko@gmail.com>
2019-07-30 20:49:54 +0300

    Feature: todo mvc [ref:no-ref]

--- lesson-5/index-1.js
+++ lesson-5/index-1.js
@@ -6,23 +6,29 @@ generateTodo(todoApp);
 function generateTodo(element) {
     const input = element.querySelector('.new-todo');
     const ul = element.querySelector('.todo-list');
+    const counter = element.querySelector('.todo-count strong');
+    const filters = element.querySelector('.filters');
+    counter.addEventListener('updatecount', (e) => {
+        e.currentTarget.innerHTML = e.detail;
+    });
     ul.addEventListener('click', (e) => {
         const el = e.target;
         const isRemove = el.classList.contains('destroy');
         const isComplete = el.classList.contains('toggle');
-        if (!isRemove || !isComplete) {
+        if (!isRemove && !isComplete) {
             return;
         }
         const li = el.closest('li');
         if (isRemove) {
-            ul.removeChild(li)
+            ul.removeChild(li);
+            dispatch(counter, 'updatecount');
             return;
         }
         li.classList.add('complete');
     });
     input.addEventListener('keydown', (event) => {
         const {code, target} = event;
-        if (code !== 'Enter') {
+        if (code !== 'Enter' || !target.value) {
             return;
         }
         ul.insertAdjacentHTML('beforeend',
@@ -32,5 +38,42 @@ function generateTodo(element) {
                 </div>
                 <input type="text" class="edit"></li>`);
         target.value = '';
+        dispatch(counter, 'updatecount')
     });
+    filters.addEventListener('click', (event) => {
+        const list = document.querySelectorAll('.todo-list li');
+        for (let li of list) {
+            switch (event.target.className) {
+                case 'all' : {
+                    li.removeAttribute('hidden');
+                    break;
+                }
+                case 'active' : {
+                    if (li.classList.contains('complete')) {
+                        li.setAttribute('hidden', true);
+                        break
+                    }
+                    li.removeAttribute('hidden');
+                    break;
+                }
+                case 'completed' : {
+                    if (li.classList.contains('complete')) {
+                        li.removeAttribute('hidden');
+                        break
+                    }
+                    li.setAttribute('hidden', true);
+                    break;
+                }
+            }
+        }
+        dispatch(counter, 'updatecount');
+    })
+}
+
+
+function dispatch(el, eventName) {
+    const list = [...document.querySelectorAll('.todo-list li')]
+        .filter((li) => !li.hasAttribute('hidden'));
+    const e = new CustomEvent(eventName, {detail: list.length});
+    el.dispatchEvent(e);
 }
